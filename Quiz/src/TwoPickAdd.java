a0084c5a9e0f73d65bcde438ce5a099755c07983
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-11-14 00:13:43 +0900

    Programmers 두 개 뽑아서 더하기

--- Quiz/src/TwoPickAdd.java
+++ Quiz/src/TwoPickAdd.java
@@ -1,41 +1,24 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 
 public class TwoPickAdd {
     public static void main(String[] args) {
         int[] number = {2, 1, 3, 4, 1};
-        int[] answer = {};
         ArrayList<Integer> result = new ArrayList();
-        boolean[] visited = new boolean[number.length];
-        comb(number, visited, 0, number.length, 2);
-        Collections.sort(result);
-    }
-
-    static void comb(int[] arr, boolean[] visited, int depth, int n, int r) {
-        if (r == 0) {
-            add(arr, visited, n);
-            return;
-        }
-        if (depth == n) {
-            return;
-        } else {
-            visited[depth] = true;
-            comb(arr, visited, depth + 1, n, r - 1);
-
-            visited[depth] = false;
-            comb(arr, visited, depth + 1, n, r);
-        }
-    }
-
-    static void add(int[] arr, boolean[] visited, int n) {
-        for (int i = 0; i < n; i++) {
-            int result = 0;
-            if (visited[i]) {
-                result += arr[i];
-                System.out.print(result);
+        for(int i=0; i<number.length; i++){
+            for(int j=i+1; j<number.length; j++){
+                int sum = number[i] + number[j];
+                if(result.indexOf(sum) <0){
+                    result.add(sum);
+                }
             }
         }
-        System.out.println();
+        System.out.print(result);
+        int[] answer = new int[result.size()];
+        for (int i=0; i<result.size(); i++){
+            answer[i] = result.get(i);
+        }
+        Arrays.sort(answer);
     }
 }
