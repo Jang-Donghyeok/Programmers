6d095aa315874e4b238bacdb48f3b007f44853e1
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-11-16 01:26:58 +0900

    Programmers 예산

--- Quiz/src/Budget.java
+++ Quiz/src/Budget.java
@@ -1,23 +1,35 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Budget {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int answer = 0;
-        int sum =0;
-        int budget=9;
-        int[] d= {1,3,2,5,4};
+        int budget = 9;
+        int sum = 0;
+        int[] d = {1, 3, 2, 5, 4};
         ArrayList<Integer> add = new ArrayList<>();
-        for (int i=0; i<d.length; i++){
+        for (int i = 0; i < d.length; i++) {
             add.add(d[i]);
         }
-        while(sum < budget){
-            if(sum >= answer){
-                continue;
-            }
-            else {
-                sum += add.get(answer);
-            }
-
+        Collections.sort(add);
+        if (sum <= budget){
+            answer = d.length;
         }
+        sum = 0;
+        if(sum > budget){
+            answer = 0;
+        }else if(sum == budget){
+            answer = 1;
+        }else {
+            while(sum < budget){
+                sum += add.get(answer);
+                if(sum > budget){
+                    continue;
+                }else{
+                    answer++;
+                }
+            }
+        }
+        System.out.print(answer);
     }
 }
