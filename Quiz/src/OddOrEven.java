34cfe67ae70416972924b8709386357a8211e5fa
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-11-28 15:43:44 +0900

    Programmers 짝수와 홀수

---
+++ Quiz/src/OddOrEven.java
@@ -0,0 +1,11 @@
+public class OddOrEven {
+    public static void main(String[] args){
+        int num = 3;
+        String answer;
+        if(num % 2 == 0){
+            answer = "Even";
+        }else {
+            answer = "Odd";
+        }
+    }
+}
