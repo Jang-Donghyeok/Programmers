025a46db28031d35a026d251bf2aff3f3f0ae8cb
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-11-30 15:55:54 +0900

    Programmers 약수의 합

--- Quiz/src/DivisorSum.java
+++ Quiz/src/DivisorSum.java
@@ -2,11 +2,12 @@ public class DivisorSum {
     public static void main(String[] args){
         int n = 12;
         int answer = 0;
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=n/2; i++){
             if(n % i == 0){
                 answer += i;
             }
         }
+        answer += n;
         System.out.println(answer);
     }
 }
