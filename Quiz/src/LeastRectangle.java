1929c102e2d25471d3206dad585a57ced62cf969
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2021-12-09 22:11:00 +0900

    Programmers 최소직사각형

--- Quiz/src/LeastRectangle.java
+++ Quiz/src/LeastRectangle.java
@@ -28,3 +28,16 @@ public class LeastRectangle {
         System.out.println(answer);
     }
 }
+/*
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int length = 0, height = 0;
+        for (int[] card : sizes) {
+            length = Math.max(length, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
+        }
+        int answer = length * height;
+        return answer;
+    }
+} */
