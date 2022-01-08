76b3237a19bfdc05308cf6e3a4c0c358cd99c79a
Jang-Donghyeok <68702805+Jang-Donghyeok@users.noreply.github.com>
2022-01-08 21:03:17 +0900

    Programmers [3차] 파일명 정리

--- Quiz/src/FileNameSort.java
+++ Quiz/src/FileNameSort.java
@@ -2,9 +2,10 @@ public class FileNameSort {
     public static void main(String[] args){
         String[] files = {"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"};
         String[] answer = {};
+        int index = 0;
         for(int i=0; i<files.length; i++){
             answer[i] = files[i];
         }
-
+        System.out.println(answer);
     }
 }
